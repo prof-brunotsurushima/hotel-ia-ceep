12518b3a6567b8fe60dd27eee134f390a00d78cc
prof-brunotsurushima <bruno.tsurushima@escola.pr.gov.br>
2026-09-22 19:14:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,23 @@
-self.addEventListener('fetch', function(event)){
-}
+const CACHE_NAME = 'meu-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Instalação do Service Worker e cache dos arquivos iniciais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercepta as requisições e entrega do cache se offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
